3711ef7e36aa08c8cff1ca3c25490767b49bb0e8
Skarm Ghelmaad <skarm.ghelmaad@gmail.com>
2022-10-30 13:49:22 +0100

    - Added new concealment descriptors (Lowlight & Darkness)

---
+++ TomeOfDarkness/MechanicsChanges/ConcealmentDescriptorToD.cs
@@ -0,0 +1,22 @@
+﻿using Kingmaker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TabletopTweaks.Core.NewUnitParts.CustomStatTypes;
+
+namespace TomeOfDarkness.MechanicsChanges
+{
+    internal class ConcealmentDescriptorToD
+    {
+        public static class NewConcealmentDescriptor
+        {
+            //The last ConcealmentDescriptor in Kingmaker.Enums.ConcealmentDescriptor is "WindsOfVengenance" [ 0x00000005 = 5 ]
+            public const ConcealmentDescriptor LowLight = (ConcealmentDescriptor)10_000;
+            public const ConcealmentDescriptor Darkness = (ConcealmentDescriptor)10_001;
+
+        }
+
+    }
+}
