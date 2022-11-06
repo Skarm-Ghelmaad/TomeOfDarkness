7b04134b91df2d0300ec07b3ec4840cb13704cd4
Skarm Ghelmaad <skarm.ghelmaad@gmail.com>
2022-11-06 15:39:36 +0100

    Added the following: - AbilityAreaEffectRunActionWithDelay

--- TomeOfDarkness/NewComponents/AreaEffects/AbilityAreaEffectRunActionWithDelay.cs
+++ TomeOfDarkness/NewComponents/AreaEffects/AbilityAreaEffectRunActionWithDelay.cs
@@ -1,172 +1,159 @@
-﻿//using Kingmaker;
-//using Kingmaker.ElementsSystem;
-//using Kingmaker.EntitySystem.Entities;
-//using Kingmaker.UnitLogic.Abilities.Components.AreaEffects;
-//using Kingmaker.UnitLogic.Abilities.Components.Base;
-//using Kingmaker.UnitLogic.Mechanics;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Runtime.Remoting.Contexts;
-//using System.Text;
-//using System.Threading.Tasks;
-//using TabletopTweaks.Core.Utilities;
+﻿using Kingmaker;
+using Kingmaker.ElementsSystem;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.UnitLogic.Abilities.Components.AreaEffects;
+using Kingmaker.UnitLogic.Abilities.Components.Base;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using TabletopTweaks.Core.Utilities;
+using UnityEngine;
 
-//namespace TomeOfDarkness.NewComponents.AreaEffects
-//{
-//    // The original component from Holic75 was "AbilityAreaEffectRunActionWithFirstRound", but I wanted a more versatile component that allowed for more fine-tuned
-//    // (and longer) delay.
-//    public class AbilityAreaEffectRunActionWithDelay : AbilityAreaEffectLogic
-//    {
-//        public override void OnUnitEnter(MechanicsContext context, AreaEffectEntityData areaEffect, UnitEntityData unit)
-//        {
-//            if (this.OnsetTimeSet == false)
-//            {
-//                this.SetOnsetTime(areaEffect);
-//                this.OnsetTimeSet = true;
-//            }
-//            if (!this.UnitEnter.HasActions || !this.IsOnsetTimeExpired() )
-//            {
-//                return;
-//            }
-//            else
-//            {
-//                if (!this.UnitEnter.HasActions)
-//                {
-//                    return;
-//                }
-//                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
-//                {
-//                    using (context.GetDataScope(unit))
-//                    {
-//                        this.UnitEnter.Run();
-//                    }
-//                }
-//            }
-//        }
+namespace TomeOfDarkness.NewComponents.AreaEffects
+{
+    // The original component from Holic75 was "AbilityAreaEffectRunActionWithFirstRound", but I wanted a more versatile component that allowed for more fine-tuned
+    // (and longer) delay.
+    public class AbilityAreaEffectRunActionWithDelay : AbilityAreaEffectLogic
+    {
+        public override void OnUnitEnter(MechanicsContext context, AreaEffectEntityData areaEffect, UnitEntityData unit)
+        {
 
-//        public override void OnUnitExit(MechanicsContext context, AreaEffectEntityData areaEffect, UnitEntityData unit)
-//        {
-//            if (this.OnsetTimeSet == false)
-//            {
-//                this.SetOnsetTime(areaEffect);
-//                this.OnsetTimeSet = true;
-//            }
-//            if (!this.UnitExit.HasActions || !this.IsOnsetTimeExpired())
-//            {
-//                return;
-//            }
-//            else
-//            {
-//                if (!this.UnitExit.HasActions)
-//                {
-//                    return;
-//                }
-//                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
-//                {
-//                    using (context.GetDataScope(unit))
-//                    {
-//                        this.UnitExit.Run();
-//                    }
-//                }
-//            }
+            if (!this.UnitEnter.HasActions || this.IsDelayRound(areaEffect))
+            {
+                return;
+            }
+            if (this.UnitEnter.HasActions && (!this.IsDelayRound(areaEffect)))
+            {
+                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
+                {
+                    using (context.GetDataScope(unit))
+                    {
+                        this.UnitEnter.Run();
+                    }
+                }
+            }
 
-//        }
+        }
 
-//        public override void OnUnitMove(MechanicsContext context, AreaEffectEntityData areaEffect, UnitEntityData unit)
-//        {
-//            if (this.OnsetTimeSet == false)
-//            {
-//                this.SetOnsetTime(areaEffect);
-//                this.OnsetTimeSet = true;
-//            }
-//            if (!this.UnitMove.HasActions || !this.IsOnsetTimeExpired())
-//            {
-//                return;
-//            }
-//            else
-//            {
-//                if (!this.UnitMove.HasActions)
-//                {
-//                    return;
-//                }
-//                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
-//                {
-//                    using (context.GetDataScope(unit))
-//                    {
-//                        this.UnitMove.Run();
-//                    }
-//                }
-//            }
-//        }
+        public override void OnUnitExit(MechanicsContext context, AreaEffectEntityData areaEffect, UnitEntityData unit)
+        {
+            if (!this.UnitExit.HasActions || this.IsDelayRound(areaEffect))
+            {
+                return;
+            }
+            if (this.UnitExit.HasActions && (!this.IsDelayRound(areaEffect)))
+            {
+                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
+                {
+                    using (context.GetDataScope(unit))
+                    {
+                        this.UnitExit.Run();
+                    }
+                }
+            }
 
-//        public override void OnRound(MechanicsContext context, AreaEffectEntityData areaEffect)
-//        {
-//            if (this.OnsetTimeSet == false)
-//            {
-//                this.SetOnsetTime(areaEffect);
-//                this.OnsetTimeSet = true;
-//            }
-//            if (!this.Round.HasActions && !this.PreOnsetRound.HasActions)
-//            {
-//                return;
-//            }
-//            else
-//            {
-//                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
-//                {
-//                    foreach (UnitEntityData unit in areaEffect.InGameUnitsInside)
-//                    {
-//                        using (context.GetDataScope(unit))
-//                        {
-//                            if (this.IsOnsetTimeExpired())
-//                            {
-//                                if (this.Round.HasActions)
-//                                {
-//                                    this.Round.Run();
-//                                }
-//                            }
-//                            else
-//                            {
-//                                if (this.PreOnsetRound.HasActions)
-//                                {
-//                                    this.PreOnsetRound.Run();
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        }
+
+        public override void OnUnitMove(MechanicsContext context, AreaEffectEntityData areaEffect, UnitEntityData unit)
+        {
+            if (!this.UnitMove.HasActions || this.IsDelayRound(areaEffect))
+            {
+                return;
+            }
+            if (this.UnitMove.HasActions && (!this.IsDelayRound(areaEffect)))
+            {
+                using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
+                {
+                    using (context.GetDataScope(unit))
+                    {
+                        this.UnitMove.Run();
+                    }
+                }
+            }
+        }
+
+        public override void OnRound(MechanicsContext context, AreaEffectEntityData areaEffect)
+        {
+            if (!this.Round.HasActions && !this.DelayRound.HasActions)
+            {
+                return;
+            }
+            using (ContextData<AreaEffectContextData>.Request().Setup(areaEffect))
+            {
+                foreach (UnitEntityData unit in areaEffect.InGameUnitsInside)
+                {
+                    using (context.GetDataScope(unit))
+                    {
+                        if (!this.IsDelayRound(areaEffect))
+                        {
+                            if (this.Round != null)
+                            {
+                                this.Round.Run();
+
+                            }
+                        }
+                        else
+                        {
+                            if (this.DelayRound != null)
+                            {
+                                this.DelayRound.Run();
+
+                            }
+                        }
+
+                    }
+                }
+            }
+            
+        }
 
 
-//        public bool IsOnsetTimeExpired()
-//        {
-//            return !(OnsetTime > Game.Instance.TimeController.GameTime);
+        public bool IsDelayRound(AreaEffectEntityData areaEffect)
+        {
+            return this.OnsetTime(areaEffect) > Game.Instance.TimeController.GameTime;
 
-//        }
+        }
 
-//        public void SetOnsetTime(AreaEffectEntityData areaEffect)
-//        {
 
-//            this.OnsetTime = areaEffect.m_CreationTime + this.Delay.Calculate(areaEffect.Context).Seconds;
+        public TimeSpan OnsetTime(AreaEffectEntityData areaEffect)
+        {
+            var delay = this.Delay.Calculate(areaEffect.Context);
 
-//        }
+            if (!this.IsNotFirstRoundDelay)
+            {
+                return areaEffect.m_CreationTime.Add(new TimeSpan(0, 0, 0, 0, 100));
+            }
+            else if (delay <= 1.Rounds())
+            {
+                return areaEffect.m_CreationTime.Add(new TimeSpan(0, 0, 0, 0, 100));
+            }
+            else
+            {
+                return areaEffect.m_CreationTime.Add(delay.Seconds).Add(new TimeSpan(0, 0, 0, 0, 100));
+            }                      
 
-//        public ActionList UnitEnter = Helpers.CreateActionList(null);
+        }
 
-//        public ActionList UnitExit = Helpers.CreateActionList(null);
+        public ActionList UnitEnter = Helpers.CreateActionList(null);
 
-//        public ActionList UnitMove = Helpers.CreateActionList(null);
+        public ActionList UnitExit = Helpers.CreateActionList(null);
 
-//        public ActionList Round = Helpers.CreateActionList(null);
+        public ActionList UnitMove = Helpers.CreateActionList(null);
 
-//        public ActionList PreOnsetRound = Helpers.CreateActionList(null);
+        public ActionList Round = Helpers.CreateActionList(null);
 
-//        public ContextDurationValue Delay;
+        public ActionList DelayRound = Helpers.CreateActionList(null);
 
-//        private TimeSpan OnsetTime;
+        public bool IsNotFirstRoundDelay = false;
 
-//        private bool OnsetTimeSet = false;
-//    }
-//}
+        [HideInInspector]
+        [ShowIf("IsNotFirstRoundDelay")]
+        public ContextDurationValue Delay;
+
+    }
+}
